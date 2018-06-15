2405eec80562f48930e88c5270b8d2e07f705de6
Hitler <a85504@alunos.uminho.pt>
2018-06-15 17:11:50 +0100

    Add da ficha6

--- ficha6.c
+++ ficha6.c
@@ -66,3 +66,22 @@ int isEmptyQ(QUEUE *q)
     return q->tamanho;
 }
 
+
+int main()
+{
+    int i, z;
+    STACK s;
+    initStack(&s);
+    push(&s, 10);
+    push(&s, 20);
+    push(&s, 30);
+    for(i = 0; i < s.sp; i++)
+        printf("%d ", s.valores[i]);
+    pop(&s, &z);
+    for(i = 0; i < s.sp; i++)
+        printf("%d ", s.valores[i]);
+    printf("\n%d\n", z);
+    top(&s, &z);
+    printf("%d\n", z);
+    return 0;
+}
